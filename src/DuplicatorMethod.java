64087846d62e8320b3f6987b42f1d7d95a76a3ec
Matt He <matt_hlh@hotmail.com>
2018-09-16 20:05:59 -0700

    Made program that calculates the mean, median and mode and also made a method that is suppose to duplicate rows of two day array, but it doesn't function yet

--- src/DuplicatorMethod.java
+++ src/DuplicatorMethod.java
@@ -1,26 +1,29 @@
 public class DuplicatorMethod{
     public static void main(String args[]) {
         int[][] twoDArray = {{1, 3, 5, 8},
-                {2, 5, 7, 9},
-                {5, 8, 3, 5},
-                {3, 5, 7, 1}};
+                             {2, 5, 7, 9},
+                             {5, 8, 3, 5},
+                             {3, 5, 7, 1},};
         twoDArray = duplicaterows(twoDArray);
         for(int i = 0; i < twoDArray.length; i++){
             for(int j = 0; j < twoDArray[i].length; j++){
-                System.out.print(twoDArray[j][i]);
+                System.out.print(twoDArray[i][j]);
             }
+            System.out.println();
         }
     }
     public static int[][] duplicaterows(int[][] array){
-        for(int row = 0; row < array.length; row++){
-            for(int col = 0; col < array[row].length; col++){
-                int num = array[col][row];
-                if((col) == (array.length - 1)){
-                    return array;
-                } else{
-                    array[col + 1][row] = num;
+        int count = 0;
+        int num;
+        while(count < array.length){
+            if(count % 2 == 0){
+                for(int row  = 0; row < array.length; row++){
+                    num = array[count][row];
+                    array[count + 1][row] = num;
                 }
+                count = count + 2;
             }
+            count++;
         }
         return array;
     }

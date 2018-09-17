318baec4fb0dbe858eaec465f3cbdba2f1ed1fe3
Kelvin Zhang <me@kelvinzhang.ca>
2018-09-16 20:19:40 -0700

    Fixed 2D Array Duplicator

--- src/DuplicatorMethod.java
+++ src/DuplicatorMethod.java
@@ -1,29 +1,28 @@
 public class DuplicatorMethod{
     public static void main(String args[]) {
-        int[][] twoDArray = {{1, 3, 5, 8},
-                             {2, 5, 7, 9},
-                             {5, 8, 3, 5},
-                             {3, 5, 7, 1},};
-        twoDArray = duplicaterows(twoDArray);
-        for(int i = 0; i < twoDArray.length; i++){
-            for(int j = 0; j < twoDArray[i].length; j++){
+        int[][] twoDArray = {{1, 5, 4, 8, 7, 3},
+                             {2, 4, 3, 5, 7, 6},
+                             {2, 4, 3, 3, 6, 0},
+                             {9, 8, 9, 9, 4, 1}};
+        twoDArray = duplicateRows(twoDArray);
+        for (int i = 0; i < twoDArray.length; i++) {
+            for (int j = 0; j < twoDArray[i].length; j++) {
                 System.out.print(twoDArray[i][j]);
             }
             System.out.println();
         }
     }
-    public static int[][] duplicaterows(int[][] array){
-        int count = 0;
+    public static int[][] duplicateRows(int[][] array) {
+        int row = 0;
         int num;
-        while(count < array.length){
-            if(count % 2 == 0){
-                for(int row  = 0; row < array.length; row++){
-                    num = array[count][row];
-                    array[count + 1][row] = num;
+        while (row < array.length) {
+            if (row % 2 == 0) {
+                for (int col  = 0; col < array[row].length; col++) {
+                    num = array[row][col];
+                    array[row + 1][col] = num;
                 }
-                count = count + 2;
             }
-            count++;
+            row++;
         }
         return array;
     }

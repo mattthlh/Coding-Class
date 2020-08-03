411921b3d2669fa4bd0ec12fa822d57895dedf1b
Matt He <matt_hlh@hotmail.com>
2020-08-03 11:37:46 -0700

    Created solution for the CCC 2013 S4 questions

--- src/ContestQuestions/CCC13S4.java
+++ src/ContestQuestions/CCC13S4.java
@@ -12,31 +12,64 @@ public class CCC13S4 {
     static int m;
     static ArrayList<Integer>[] adj;
     static boolean visited[];
-    static boolean found = false;
+    static boolean found;
+    static int target;
 
     public static void main(String[] args) throws IOException {
         in = new BufferedReader(new InputStreamReader(System.in));
 
-        String line = in.readLine();
+        String firstLine = in.readLine();
 
-        n = Integer.parseInt(line.split(" ")[0]);
-        m = Integer.parseInt(line.split(" ")[1]);
+        n = Integer.parseInt(firstLine.split(" ")[0]);
+        m = Integer.parseInt(firstLine.split(" ")[1]);
+
         adj = new ArrayList[n + 1];
         visited = new boolean[n + 1];
-        for(int i = 1; i < n; i++) {
-            adj[i] = new ArrayList<Integer>();
+        for(int i = 1; i <= n; i++) {
+            adj[i] = new ArrayList<>();
         }
 
+        for(int i = 0; i < m; i++) {
+            String[] nextMLines = in.readLine().split(" ");
+            int x = Integer.parseInt(nextMLines[0]);
+            int y = Integer.parseInt(nextMLines[1]);
+
+            adj[x].add(y);
+        }
+        String[] lastLine = in.readLine().split(" ");
+
+        int p = Integer.parseInt(lastLine[0]);
+        int q = Integer.parseInt(lastLine[1]);
+
+        target = q;
+        found = false;
+        dfs(p);
+
+        if(found) {
+            System.out.println("yes");
+            return;
+        }
+
+        target = p;
+        found = false;
+        visited = new boolean[n + 1];
+        dfs(q);
+
+        if (found) {
+            System.out.println("no");
+            return;
+        }
+        System.out.println("unknown");
     }
-    public static void dfs(int current, int target) {
+    public static void dfs(int current) {
         if(current == target) {
             found = true;
         }
         visited[current] = true;
         for(int to : adj[current]) {
-            if(visited[to]) continue;
-
-            dfs(to, target);
+            if(!visited[to]) {
+                dfs(to);
+            }
         }
     }
 }
